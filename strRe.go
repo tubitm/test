2e46eed969cdf50d01f1ac0090b70d59b4637812
tubitm <75247419+tubitm@users.noreply.github.com>
2020-12-06 14:33:23 +0700

    Update strRe.go

--- strRe.go
+++ strRe.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println(strings.Replace("Hello", "l", "z", 2))
 	fmt.Println(strings.Replace("Wednesday", "e", "xx", -1))
 	fmt.Println(strings.Replace("thapthim Santhi", "th", "999", -1))
+	fmt.Println(strings.Replace("2521233", "2", "8", 2))
 }
