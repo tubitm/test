638cc1c6203a489477cdea073b197d731ba4c60c
tubitm <75247419+tubitm@users.noreply.github.com>
2020-12-06 13:56:20 +0700

    Update Index.go

--- Index.go
+++ Index.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println(strings.Index("Hello World", "hello"))
 	fmt.Println(strings.Index("sunday", "day"))
 	fmt.Println(strings.Index("John H. Watson", "watson"))
+	fmt.Println(strings.Index("John H. Watson", "."))
 }
