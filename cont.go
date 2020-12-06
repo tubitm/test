0c18a143be2d54816d675f5bdd2b7b3ea8a4ed0d
tubitm <75247419+tubitm@users.noreply.github.com>
2020-12-06 13:24:09 +0700

    Update cont.go

--- cont.go
+++ cont.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	fmt.Println(strings.Count("ComPro", "o"))
 	fmt.Println(strings.Count("Hello World", "l"))
+	fmt.Println(strings.Count("Hello World", ""))
 	fmt.Println(strings.Count("SunDay", "d"))
 }
